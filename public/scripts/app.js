542323219e0bc5ca93a87dc83fad78df25f55fcb
Krystian <kortower@gmail.com>
2016-08-19 15:41:24 +0200

    update 19.08.2016
    fix margin & add js function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -16,5 +16,37 @@ $(".tree").click(function () {
     }, 1000);
 });
 
+$(window).scroll(function () {
+    var wScroll = $(this).scrollTop(),
+        where = $(this).scrollTop();
 
+    if(wScroll > $('.contact').offset().top - ($(window).height() / 1.2)) {
+       $('.contact .contact-form').each(function () {
+           setTimeout(function () {
+               $('.contact .contact-form').addClass('is-showing');
+           }, 300);
+       });
+     }
 
+     if(where > 0 && where < $('.page_gallery').offset().top - ($(window).height() / 1.2)) {
+       $('.global .global_navigation').each(function(){
+         $('.global .global_navigation').addClass('page-one');
+         $('.global .global_navigation').removeClass('page-two');
+         $('.global .global_navigation').removeClass('page-tree');
+       });
+     }
+     if(where > $('.page_gallery').offset().top - ($(window).height() / 3) && where <  $('.contact').offset().top - ($(window).height() / 1.2)) {
+       $('.global .global_navigation').each(function(){
+         $('.global .global_navigation').addClass('page-two');
+         $('.global .global_navigation').removeClass('page-tree');
+         $('.global .global_navigation').removeClass('page-one');
+       });
+     }
+     if(where > $('.contact').offset().top - ($(window).height() / 3)) {
+       $('.global .global_navigation').each(function(){
+         $('.global .global_navigation').addClass('page-tree');
+         $('.global .global_navigation').removeClass('page-one');
+         $('.global .global_navigation').removeClass('page-two');
+       });
+     }
+});
